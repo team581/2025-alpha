8993a70fdd0865ceece04b1b392595062de2699e
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-01-18 10:32:51 -0800

    pipe pose values rounded down to 4 decimal point

--- src/main/java/frc/robot/auto_align/ReefPipe.java
+++ src/main/java/frc/robot/auto_align/ReefPipe.java
@@ -6,41 +6,46 @@ import frc.robot.fms.FmsSubsystem;
 
 public enum ReefPipe {
   PIPE_A(
-      new Pose2d(3.7086032, 3.8616128, new Rotation2d(Math.PI)),
-      new Pose2d(13.8396472, 4.1901872, new Rotation2d(0.0))),
+      new Pose2d(3.7086, 3.8616, new Rotation2d(Math.PI)),
+      new Pose2d(13.8396, 4.1902, new Rotation2d(0.0))),
   PIPE_B(
-      new Pose2d(3.7086032, 4.190238, new Rotation2d(Math.PI)),
-      new Pose2d(13.8396472, 3.861562, new Rotation2d(0.0))),
+      new Pose2d(3.7086, 4.1902, new Rotation2d(Math.PI)),
+      new Pose2d(13.8396, 3.8616, new Rotation2d(0.0))),
   PIPE_C(
-      new Pose2d(3.95667691423813, 4.619914277082763, new Rotation2d(2.0943951023931953)),
-      new Pose2d(13.591573485761872, 3.4318857229172375, new Rotation2d(-1.047197551196598))),
+      new Pose2d(3.9567, 4.6199, new Rotation2d(2.0944)),
+      new Pose2d(13.5916, 3.4319, new Rotation2d(-1.0472))),
   PIPE_D(
-      new Pose2d(4.241274685761871, 4.784226877082762, new Rotation2d(2.0943951023931953)),
-      new Pose2d(13.30697571423813, 3.267573122917238, new Rotation2d(-1.047197551196598))),
+      new Pose2d(4.2413, 4.7842, new Rotation2d(2.0944)),
+      new Pose2d(13.3070, 3.2676, new Rotation2d(-1.0472))),
   PIPE_E(
-      new Pose2d(4.73742211423813, 4.784226877082762, new Rotation2d(1.0471975511965976)),
-      new Pose2d(12.810828285761872, 3.267573122917238, new Rotation2d(-2.0943951023931957))),
+      new Pose2d(4.7374, 4.7842, new Rotation2d(1.0472)),
+      new Pose2d(12.8108, 3.2676, new Rotation2d(-2.0944))),
   PIPE_F(
-      new Pose2d(5.022019885761871, 4.619914277082763, new Rotation2d(1.0471975511965976)),
-      new Pose2d(12.52623051423813, 3.4318857229172375, new Rotation2d(-2.0943951023931957))),
+      new Pose2d(5.0220, 4.6199, new Rotation2d(1.0472)),
+      new Pose2d(12.5262, 3.4319, new Rotation2d(-2.0944))),
   PIPE_G(
-      new Pose2d(5.2700936, 4.190238, new Rotation2d(0.0)),
-      new Pose2d(12.2781568, 3.861562, new Rotation2d(Math.PI))),
+      new Pose2d(5.2701, 4.1902, new Rotation2d(0.0)),
+      new Pose2d(12.2782, 3.8616, new Rotation2d(Math.PI))),
   PIPE_H(
-      new Pose2d(5.2700936, 3.8616128, new Rotation2d(0.0)),
-      new Pose2d(12.2781568, 4.1901872, new Rotation2d(Math.PI))),
+      new Pose2d(5.2701, 3.8616, new Rotation2d(0.0)),
+      new Pose2d(12.2782, 4.1902, new Rotation2d(Math.PI))),
   PIPE_I(
-      new Pose2d(5.022019885761871, 3.43193652291724, new Rotation2d(-1.0471975511965976)),
-      new Pose2d(12.52623051423813, 4.619863477082762, new Rotation2d(2.0943951023931957))),
+      new Pose2d(5.0220, 3.4319, new Rotation2d(-1.04720)),
+      new Pose2d(12.5262, 4.6199, new Rotation2d(2.0944))),
   PIPE_J(
-      new Pose2d(4.73742211423813, 3.26762392291724, new Rotation2d(-1.0471975511965976)),
-      new Pose2d(12.810828285761872, 4.784176077082762, new Rotation2d(2.0943951023931957))),
+      new Pose2d(4.7374, 3.2676, new Rotation2d(-1.04720)),
+      new Pose2d(12.8108, 4.7842, new Rotation2d(2.0944))),
   PIPE_K(
-      new Pose2d(4.241274685761871, 3.26762392291724, new Rotation2d(-2.0943951023931953)),
-      new Pose2d(13.30697571423813, 4.784176077082762, new Rotation2d(1.0471975511965979))),
+      new Pose2d(4.2413, 3.2676, new Rotation2d(-2.09440)),
+      new Pose2d(13.3070, 4.7842, new Rotation2d(1.0472))),
   PIPE_L(
-      new Pose2d(3.95667691423813, 3.43193652291724, new Rotation2d(-2.0943951023931953)),
-      new Pose2d(13.591573485761872, 4.619863477082762, new Rotation2d(1.0471975511965979)));
+      new Pose2d(3.9567, 3.4319, new Rotation2d(-2.09440)),
+      new Pose2d(13.5916, 4.6199, new Rotation2d(1.0472)));
+
+  // These are the poses for the tip of each vertical pipe on the reef
+  // PIPE_A is on the reef side closest to alliance area, it is the leftmost bottom pipe
+  // The following pipes are in order clockwise, with PIPE_B being above PIPE_A, and PIPE_C on the following reef side
+  // For each pipe there has been 2 poses inputted, first one is the pipes on the reef of the blue side of the field and the red side
 
   public final Pose2d redPose;
   public final Pose2d bluePose;
