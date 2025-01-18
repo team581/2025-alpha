871ab47ab5931520855da3135abe05c039f6f3a9
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2025-01-18 13:35:17 -0800

    confirmed reefpipe poses

--- src/test/java/frc/robot/auto_align/ReefPipeTest.java
+++ src/test/java/frc/robot/auto_align/ReefPipeTest.java
@@ -3,18 +3,15 @@ package frc.robot.auto_align;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import frc.robot.util.MathHelpers;
 import org.junit.jupiter.api.Test;
 
 public class ReefPipeTest {
-  private static final Translation2d BLUE_REEF_CENTER = new Translation2d(4.4893, 4.0259);
-  private static final Translation2d RED_REEF_CENTER = new Translation2d(13.0589504, 4.0259);
+  private static final Translation2d BLUE_REEF_CENTER = new Translation2d(4.4789, 4.0259);
+  private static final Translation2d RED_REEF_CENTER = new Translation2d(13.0447, 4.0259);
 
-  private static final double BLUE_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE =
-      Units.inchesToMeters(145.6);
-  private static final double RED_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE =
-      Units.inchesToMeters(544.305848);
+  private static final double BLUE_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE = 3.6982;
+  private static final double RED_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE = 13.8254;
 
   private static double round(double value) {
     // Round to centimeter precision
@@ -45,19 +42,13 @@ public class ReefPipeTest {
 
   @Test
   void checkBlueReefPipeADistanceToWall() {
-    // 1cm of fudge factor because the numbers in code are slightly off from the field CAD
-    // TODO: Fix 1cm of error
     assertEquals(
-        round(BLUE_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE + 0.01),
-        round(ReefPipe.PIPE_A.bluePose.getX()));
+        round(BLUE_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE), round(ReefPipe.PIPE_A.bluePose.getX()));
   }
 
   @Test
   void checkRedReefPipeADistanceToWall() {
-    // 1cm of fudge factor because the numbers in code are slightly off from the field CAD
-    // TODO: Fix 1cm of error
     assertEquals(
-        round(RED_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE + 0.01),
-        round(ReefPipe.PIPE_A.redPose.getX()));
+        round(RED_PIPE_A_TO_BLUE_ALLIANCE_WALL_DISTANCE), round(ReefPipe.PIPE_A.redPose.getX()));
   }
 }
