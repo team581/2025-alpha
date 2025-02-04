ed70770932409b8d17c9e99c192a0b4c5ad93945
Simon <87281874+simonp17@users.noreply.github.com>
2025-02-04 14:58:14 -0800

    Add intake assist omega speeds for posterity

--- src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -236,7 +236,7 @@ public class SwerveSubsystem extends StateMachine<SwerveState> {
                     teleopSpeeds.vxMetersPerSecond + assistSpeedsOffset.vxMetersPerSecond)
                 .withVelocityY(
                     teleopSpeeds.vyMetersPerSecond + assistSpeedsOffset.vyMetersPerSecond)
-                .withRotationalRate(teleopSpeeds.omegaRadiansPerSecond)
+                .withRotationalRate(teleopSpeeds.omegaRadiansPerSecond + assistSpeedsOffset.omegaRadiansPerSecond)
                 .withDriveRequestType(DriveRequestType.OpenLoopVoltage));
       }
       case REEF_MAGNETISM_TELEOP -> {
