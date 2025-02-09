237cc2251095c765e009995e3adde6a0398f9b68
Jonah Snider <jonah@jonahsnider.com>
2025-02-09 13:07:42 -0800

    Super scuffed climber integration

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,9 +9,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.autos.Autos;
 import frc.robot.autos.Trailblazer;
 import frc.robot.climber.ClimberSubsystem;
+import frc.robot.climber.TempClimberState;
 import frc.robot.config.RobotConfig;
 import frc.robot.controller.RumbleControllerSubsystem;
 import frc.robot.elevator.ElevatorSubsystem;
@@ -232,8 +234,18 @@ public class Robot extends TimedRobot {
     hardware.driverController.x().onTrue(robotCommands.l3LineupCommand());
     hardware.driverController.b().onTrue(robotCommands.l2LineupCommand());
     hardware.driverController.a().onTrue(robotCommands.lowLineupCommand());
-    hardware.driverController.povUp().onTrue(robotCommands.climbUpCommand());
-    hardware.driverController.povDown().onTrue(robotCommands.climbDownCommand());
+    // hardware.driverController.povUp().onTrue(robotCommands.climbUpCommand());
+    // hardware.driverController.povDown().onTrue(robotCommands.climbDownCommand());
+    hardware
+        .driverController
+        .povUp()
+        .onTrue(Commands.runOnce(() -> climber.setState(TempClimberState.UP)))
+        .onFalse(Commands.runOnce(() -> climber.setState(TempClimberState.STOPPED)));
+    hardware
+        .driverController
+        .povDown()
+        .onTrue(Commands.runOnce(() -> climber.setState(TempClimberState.DOWN)))
+        .onFalse(Commands.runOnce(() -> climber.setState(TempClimberState.STOPPED)));
     hardware
         .driverController
         .povLeft()
--- src/main/java/frc/robot/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/climber/ClimberSubsystem.java
@@ -1,47 +1,73 @@
 package frc.robot.climber;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import dev.doglog.DogLog;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.util.Units;
+import frc.robot.config.RobotConfig;
 import frc.robot.util.scheduling.SubsystemPriority;
 import frc.robot.util.state_machines.StateMachine;
 
 public class ClimberSubsystem extends StateMachine<ClimberState> {
   private static final double TOLERANCE = 1;
-  // private final TalonFX motor;
-  // private final CANcoder encoder;
+  private final TalonFX motor;
+  private final CANcoder encoder;
   private double currentAngle;
+  private TempClimberState tempState = TempClimberState.STOPPED;
 
-  public ClimberSubsystem(/* TalonFX motor, CANcoder encoder */ ) {
+  public ClimberSubsystem(TalonFX motor, CANcoder encoder) {
     super(SubsystemPriority.CLIMBER, ClimberState.STOWED);
 
     // motor.getConfigurator().apply(RobotConfig.get().climber().motorConfig());
-    // encoder.getConfigurator().apply(RobotConfig.get().climber().cancoderConfig());
+    motor
+        .getConfigurator()
+        .apply(
+            new TalonFXConfiguration()
+                .withCurrentLimits(
+                    new CurrentLimitsConfigs()
+                        .withStatorCurrentLimit(70)
+                        .withSupplyCurrentLimit(50))
+                .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake)));
+    encoder.getConfigurator().apply(RobotConfig.get().climber().cancoderConfig());
 
-    // this.motor = motor;
-    // this.encoder = encoder;
+    this.motor = motor;
+    this.encoder = encoder;
   }
 
   @Override
   public void robotPeriodic() {
     super.robotPeriodic();
 
-    // if (atGoal()) {
-    //   motor.disable();
-    // } else {
-    //   if (currentAngle < getState().angle) {
-    //     motor.setVoltage(0);
-    //   } else {
-    //     motor.setVoltage(-0);
-    //   }
-    // }
+    switch (tempState) {
+      case STOPPED -> motor.disable();
+      // case UP -> motor.setVoltage(8);
+      // case DOWN -> motor.setVoltage(-8);
+      case UP -> motor.disable();
+      case DOWN -> motor.disable();
+    }
+
+    DogLog.log("Climber/TempState", tempState);
+    DogLog.log("Climber/StatorCurrent", motor.getStatorCurrent().getValueAsDouble());
+    DogLog.log("Climber/SupplyCurrent", motor.getSupplyCurrent().getValueAsDouble());
+    DogLog.log("Climber/OutputVoltage", motor.getMotorVoltage().getValueAsDouble());
   }
 
   public void setState(ClimberState newState) {
     setStateFromRequest(newState);
   }
 
+  public void setState(TempClimberState newState) {
+    tempState = newState;
+  }
+
   @Override
   protected void collectInputs() {
-    // currentAngle = Units.rotationsToDegrees(encoder.getPosition().getValueAsDouble());
+    currentAngle = Units.rotationsToDegrees(encoder.getPosition().getValueAsDouble());
   }
 
   public boolean atGoal() {
