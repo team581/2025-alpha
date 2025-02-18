f6b1700cb322ac73027ee70c9423c927e2427288
Jonah Snider <jonah@jonahsnider.com>
2025-02-17 21:27:44 -0800

    Add ReefState stub & tests

---
+++ src/main/java/frc/robot/auto_align/ReefState.java
@@ -0,0 +1,16 @@
+package frc.robot.auto_align;
+
+public class ReefState {
+  public void clear() {
+    // TODO: Implement
+  }
+
+  public void markScored(ReefPipe pipe, ReefPipeLevel level) {
+    // TODO: Implement
+  }
+
+  public boolean isScored(ReefPipe pipe, ReefPipeLevel level) {
+    // TODO: Implement
+    return false;
+  }
+}
