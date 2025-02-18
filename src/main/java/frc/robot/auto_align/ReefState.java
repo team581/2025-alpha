92943c2c4fa14e9d102e25a99278714034456980
Simon <87281874+simonp17@users.noreply.github.com>
2025-02-18 15:04:36 -0800

    Implement reef state

--- src/main/java/frc/robot/auto_align/ReefState.java
+++ src/main/java/frc/robot/auto_align/ReefState.java
@@ -1,16 +1,45 @@
 package frc.robot.auto_align;
 
+import java.util.EnumSet;
+
 public class ReefState {
+  private EnumSet<ReefPipe> scoredL2Pipes = EnumSet.noneOf(ReefPipe.class);
+  private EnumSet<ReefPipe> scoredL3Pipes = EnumSet.noneOf(ReefPipe.class);
+  private EnumSet<ReefPipe> scoredL4Pipes = EnumSet.noneOf(ReefPipe.class);
+
+
   public void clear() {
-    // TODO: Implement
+    scoredL2Pipes.clear();
+    scoredL3Pipes.clear();
+    scoredL4Pipes.clear();
   }
 
   public void markScored(ReefPipe pipe, ReefPipeLevel level) {
-    // TODO: Implement
+    switch (level) {
+      case L2:
+        scoredL2Pipes.add(pipe);
+        break;
+      case L3:
+        scoredL3Pipes.add(pipe);
+        break;
+      case L4:
+        scoredL4Pipes.add(pipe);
+        break;
+      default:
+        break;
+    }
   }
 
   public boolean isScored(ReefPipe pipe, ReefPipeLevel level) {
-    // TODO: Implement
-    return false;
+    switch (level) {
+      case L2:
+        return scoredL2Pipes.contains(pipe);
+      case L3:
+        return scoredL3Pipes.contains(pipe);
+      case L4:
+        return scoredL4Pipes.contains(pipe);
+      default:
+        return false;
+    }
   }
 }
