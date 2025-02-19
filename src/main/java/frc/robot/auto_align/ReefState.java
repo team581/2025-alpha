2b887a2c9b3db66a8036543ff97c6d2faa61ef5a
Simon <87281874+simonp17@users.noreply.github.com>
2025-02-18 20:18:12 -0800

    Resolve comments

--- src/main/java/frc/robot/auto_align/ReefState.java
+++ src/main/java/frc/robot/auto_align/ReefState.java
@@ -3,9 +3,9 @@ package frc.robot.auto_align;
 import java.util.EnumSet;
 
 public class ReefState {
-  private EnumSet<ReefPipe> scoredL2Pipes = EnumSet.noneOf(ReefPipe.class);
-  private EnumSet<ReefPipe> scoredL3Pipes = EnumSet.noneOf(ReefPipe.class);
-  private EnumSet<ReefPipe> scoredL4Pipes = EnumSet.noneOf(ReefPipe.class);
+  private final EnumSet<ReefPipe> scoredL2Pipes = EnumSet.noneOf(ReefPipe.class);
+  private final EnumSet<ReefPipe> scoredL3Pipes = EnumSet.noneOf(ReefPipe.class);
+  private final EnumSet<ReefPipe> scoredL4Pipes = EnumSet.noneOf(ReefPipe.class);
 
   public void clear() {
     scoredL2Pipes.clear();
@@ -15,30 +15,26 @@ public class ReefState {
 
   public void markScored(ReefPipe pipe, ReefPipeLevel level) {
     switch (level) {
-      case L2:
+      case L2 ->
         scoredL2Pipes.add(pipe);
-        break;
-      case L3:
+      case L3 ->
         scoredL3Pipes.add(pipe);
-        break;
-      case L4:
+      case L4->
         scoredL4Pipes.add(pipe);
-        break;
-      default:
-        break;
+      default-> {}
     }
   }
 
   public boolean isScored(ReefPipe pipe, ReefPipeLevel level) {
-    switch (level) {
-      case L2:
-        return scoredL2Pipes.contains(pipe);
-      case L3:
-        return scoredL3Pipes.contains(pipe);
-      case L4:
-        return scoredL4Pipes.contains(pipe);
-      default:
-        return false;
-    }
+    return switch (level) {
+      case L2 ->
+          scoredL2Pipes.contains(pipe);
+      case L3->
+         scoredL3Pipes.contains(pipe);
+      case L4->
+         scoredL4Pipes.contains(pipe);
+      default->
+         false;
+    };
   }
 }
