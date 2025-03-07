0e298a3141b5e2941795b42c35e462ee4d61f0a7
Simon <87281874+simonp17@users.noreply.github.com>
2025-03-06 19:02:19 -0800

    Change intake assist

--- src/main/java/frc/robot/robot_manager/RobotManager.java
+++ src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -1012,7 +1012,8 @@ public class RobotManager extends StateMachine<RobotState> {
       }
       case INTAKE_ASSIST_CORAL_FLOOR_HORIZONTAL -> {
         if (maybeBestCoralMapTranslation.isPresent()) {
-          swerve.snapsDriveRequest(coralIntakeAssistAngle);
+          swerve.setFieldRelativeCoralAssistSpeedsOffset(IntakeAssistUtil.getAssistSpeedsFromPose(maybeBestCoralMapTranslation.get(), localization.getPose()));
+          swerve.coralAlignmentDriveRequest(coralIntakeAssistAngle);
         } else {
           swerve.normalDriveRequest();
         }
