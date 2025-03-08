d9d952cb4c3e2607f6ebfec4453798545cd9c618
Jonah Snider <jonah@jonahsnider.com>
2025-03-08 11:09:26 -0800

    Update constants for new climb test

--- src/main/java/frc/robot/climber/ClimberState.java
+++ src/main/java/frc/robot/climber/ClimberState.java
@@ -2,10 +2,10 @@ package frc.robot.climber;
 
 public enum ClimberState {
   STOWED(195, 2, -2),
-  LINEUP(0, 12, -12),
-  HANGING(156.0, 12, -12),
-  HANGING_2(160.0, 12, -12),
-  HANGING_3(165.0, 12, -12);
+  LINEUP(0, 4, -4),
+  HANGING(156.0, 4, -4),
+  HANGING_2(160.0, 4, -4),
+  HANGING_3(165.0, 4, -4);
 
   public final double angle;
   public final double forwardsVoltage;
